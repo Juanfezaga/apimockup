ba53435896bd4b827fc77e1d1b8d24426127a606
Juan Zapata <49914276+Juanfezaga@users.noreply.github.com>
2021-12-02 03:35:20 +0000

    added mockup data

--- services.js
+++ services.js
@@ -320,6 +320,629 @@ const calculatorsData = {
       }
     ]
   },
+  "interior-painting": {
+    data: [
+      {
+        name: "InteriorPaint",
+        cuantity: "6,2",
+        unit: "Cuñetes"
+      }
+    ]
+  },
+  "exterior-painting": {
+    data: [
+      {
+        name :"ExteriorPaint",
+        cuantity: "6,2",
+        unit: "Cuñetes"
+      }
+    ]
+  },
+  "drywall-ceiling": {
+    data: [
+      {
+        name: "DrywallSheet",
+        cuantity: "12,2",
+        unit: "Unidad"
+      },
+      {
+        name: "PaintTypeTwo",
+        cuantity: "1",
+        unit: "Galon"
+      },
+      {
+        name: "PlasticStucco",
+        cuantity: "1,6",
+        unit: "Cuñete"
+      },
+      {
+        name: "TwoByTwoAngle",
+        cuantity: "14,7",
+        unit: "Unidad"
+      },
+      {
+        name: "ZProfile",
+        cuantity: "5,9",
+        unit: "Unidad"
+      },
+      {
+        name: "PaperTape",
+        cuantity: "1,6",
+        unit: "Unidad"
+      },
+      {
+        name: "PlateScrew",
+        cuantity: "640",
+        unit: "Unidad"
+      },
+      {
+        name: "MeshTape",
+        cuantity: "3,2",
+        unit: "Unidad"
+      },
+    ]
+  },
+  "smooth-stucco": {
+    data: [
+      {
+        name: "SmoothStucco",
+        cuantity: "35",
+        unit: "Bulto de 25Kg"
+      }
+    ]
+  },
+  "acrylic-stucco": {
+    data: [
+      {
+        name: "PlasticStucco",
+        cuantity: "6,2",
+        unit: "Cuñete"
+      }
+    ]
+  },
+  "ceramic-floor": {
+    data: [
+      {
+        name: "CeramicFloor",
+        cuantity: "35",
+        unit: "m2"
+      },
+      {
+        name: "CeramicTileAdhesive",
+        cuantity: "7,7",
+        unit: "Kilos"
+      },
+      {
+        name: "Nozzle",
+        cuantity: "1,4",
+        unit: "Kilos"
+      },
+      {
+        name: "Gauze",
+        cuantity: "11,6",
+        unit: "Kilos"
+      },
+      {
+        name: "BagSeparators",
+        cuantity: "3,5",
+        unit: "Unidades"
+      }
+    ]
+  },
+  "porcelain-floor": {
+    data: [
+      {
+        name: "PorcelainFloor",
+        cuantity: "35",
+        unit: "m2"
+      },
+      {
+        name: "PorcelainTileAdhesive",
+        cuantity: "7,7",
+        unit: "Kilos"
+      },
+      {
+        name: "Nozzle",
+        cuantity: "1,4",
+        unit: "Kilos"
+      },
+      {
+        name: "Gauze",
+        cuantity: "11,6",
+        unit: "Kilos"
+      },
+      {
+        name: "BagSeparators",
+        cuantity: "3,5",
+        unit: "Unidades"
+      }
+    ]
+  },
+  "veneer": {
+    data: [
+      {
+        name: "VeneerPorCer",
+        cuantity: "35",
+        unit: "m2"
+      },
+      {
+        name: "FloorAdhesivePorCer",
+        cuantity: "7,7",
+        unit: "Kilos"
+      },
+      {
+        name: "Nozzle",
+        cuantity: "1,4",
+        unit: "Kilos"
+      },
+      {
+        name: "Gauze",
+        cuantity: "11,6",
+        unit: "Kilos"
+      },
+      {
+        name: "BagSeparators",
+        cuantity: "3,5",
+        unit: "Unidades"
+      }
+    ]
+  },
+  "waterprofing-coating": {
+    data: [
+      {
+        name: "Cement",
+        cuantity: "24",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "SikaOne",
+        cuantity: "34,3",
+        unit: "Kilos"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "waterprofing-coating": {
+    data: [
+      {
+        name: "Cement",
+        cuantity: "24",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "SikaOne",
+        cuantity: "34,3",
+        unit: "Kilos"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "floor-template": {
+    data: [
+      {
+        name: "Cement",
+        cuantity: "24",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "SikaOne",
+        cuantity: "34,3",
+        unit: "Kilos"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "waterproof-floor-template": {
+    data: [
+      {
+        name: "Cement",
+        cuantity: "24",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "SikaOne",
+        cuantity: "34,3",
+        unit: "Kilos"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "coating": {
+    data: [
+      {
+        name: "Cement",
+        cuantity: "24",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "SikaOne",
+        cuantity: "34,3",
+        unit: "Kilos"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "brick-block10": {
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "brick-block15": {
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "brick-block20": {
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "brick-block25": {
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "brick-personalized": {
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "ceramic-brick-brick-holes-3":{
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "ceramic-brick-brick-holes-6":{
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "ceramic-brick-brick-holes-9":{
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "ceramic-brick-brick-holes-12":{
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "ceramic-brick-personalized":{
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "solid-brick-solid":{
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "solid-brick-perforated":{
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "solid-brick-pressing":{
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
+  "solid-brick-personalized":{
+    data: [
+      {
+        name: "Blocks",
+        cuantity: "5357",
+        unit: "357"
+      },
+      {
+        name :"Cement",
+        cuantity: "9,4",
+        unit: "Bulto 50Kg"
+      },
+      {
+        name: "Sand",
+        cuantity: "3,085 171,4",
+        unit: "m3 Latas 18L"
+      },
+      {
+        name: "Water",
+        cuantity: "1249,6",
+        unit: "Litros"
+      }
+    ]
+  },
 }
 
 module.exports.calculatorsData = calculatorsData;
